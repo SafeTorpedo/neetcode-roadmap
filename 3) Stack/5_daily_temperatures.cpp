f8d77be57e2b5a0c903dd4523ff4d190c3734340
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-08-29 13:13:56 +0530

    Day 11: Daily Temperatures

---
+++ 3) Stack/5_daily_temperatures.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<pair<int, int>> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && st.top().first < temperatures[i])
+            {
+                ans[st.top().second] = i - st.top().second;
+                st.pop();
+            }
+
+            st.push({temperatures[i], i});
+        }
+        return ans;
+    }
+};
