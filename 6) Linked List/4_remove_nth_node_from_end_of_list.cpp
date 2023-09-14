7aa97105fe3d08a0d0f901ad7959793ee94cfb54
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-09-14 15:46:42 +0530

    Day 26: Remove nth Node from End of List

---
+++ 6) Linked List/4_remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *right = head;
+        ListNode *left = head;
+
+        while (n > 0)
+        {
+            right = right->next;
+            n--;
+        }
+
+        if (right == NULL)
+            return head->next;
+
+        while (right->next != NULL)
+        {
+            left = left->next;
+            right = right->next;
+        }
+
+        left->next = left->next->next;
+        return head;
+    }
+};
