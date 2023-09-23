17e30d9e6db2d7501318bb6abf60705f8d0bf0be
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-09-23 17:10:35 +0530

    Day 35: Maximum Depth of a Binary Tree

---
+++ 7) Trees/2_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// BFS Solution
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        queue<TreeNode *> q;
+        int ans = 0;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int ctr = q.size();
+            for (int i = 0; i < ctr; i++)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
+
+                if (temp->left != NULL)
+                    q.push(temp->left);
+
+                if (temp->right != NULL)
+                    q.push(temp->right);
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
+
+// Recursive DFS Solution
+/*
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
+*/
