69d35356b09e40981e084d1d4f167ca5f54ac8d7
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-10-25 13:15:27 +0530

    Day 67: Find Median from Data Stream

---
+++ 10) Heap - Priority Queue/7_find_median_from_data_stream.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MedianFinder
+{
+public:
+    MedianFinder()
+    {
+    }
+
+    void addNum(int num)
+    {
+        if (lower.empty())
+        {
+            lower.push(num);
+            return;
+        }
+
+        if (lower.size() > higher.size())
+        {
+            if (lower.top() > num)
+            {
+                higher.push(lower.top());
+                lower.pop();
+                lower.push(num);
+            }
+            else
+                higher.push(num);
+        }
+        else
+        {
+            if (num > higher.top())
+            {
+                lower.push(higher.top());
+                higher.pop();
+                higher.push(num);
+            }
+            else
+                lower.push(num);
+        }
+    }
+
+    double findMedian()
+    {
+        double ans = 0.0;
+
+        if (lower.size() == higher.size())
+            ans = (lower.top() + higher.top()) / 2.0;
+        else
+        {
+            if (lower.size() > higher.size())
+                ans = lower.top();
+            else
+                ans = higher.top();
+        }
+
+        return ans;
+    }
+
+private:
+    priority_queue<int> lower;
+    priority_queue<int, vector<int>, greater<int>> higher;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
