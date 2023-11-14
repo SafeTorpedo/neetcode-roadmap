4aaa79fff36d918f283e0eb0e0bfe142b732fb9e
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-11-14 12:58:19 +0530

    Day 87: Word Break

---
+++ 12) 1-D DP/10_word_break.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+
+        int n = s.size();
+        vector<bool> dp(n + 1);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (dp[j])
+                {
+                    string word = s.substr(j, i - j);
+                    if (words.find(word) != words.end())
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
